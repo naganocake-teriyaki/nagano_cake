3e031ea6d2c69c531586793995222eef53515560
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-02-22 07:18:28 +0000

    check point create-admin-orders

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,13 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
-  
+
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
     @order_details = @order.order_details.all
     if @order_detail.update(order_detail_params)
-      if order_detail_params[:production_status] == "in_production"
+      if order_detail_params[:making_status] == "in_production"
         @order_detail.order.update(status:2)
-      elsif @order_details.count == @order_details.where(production_status: "production_completed").count
+      elsif @order_details.count == @order_details.where(making_status: "production_completed").count
         @order_item.order.update(status:3)
       end
       redirect_to admin_order_path(@order_detail.order_id)
@@ -17,7 +17,7 @@ class Admin::OrderDetailsController < ApplicationController
 
   private
 
-  def order_item_params
-    params.require(:order_item).permit(:production_status)
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
   end
 end
