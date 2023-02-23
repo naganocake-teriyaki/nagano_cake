cb623bb7c6d59e1159e29551da216f64ae44164b
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-02-23 09:18:36 +0000

    fix order_detail.controller orders.controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,8 +10,8 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: params[:id])
-    if @order.update(order_params)
-      @order_details.update_all(making_status: 1) if @order.status == "入金確認"
+    if @order.update(order_params) == "入金確認"
+      @order_details.update_all(making_status: 1)
     end
     redirect_to admin_order_path(@order)
   end
